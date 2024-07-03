e30ad06301c9853c36bfb15bf82f40c995485050
Akbar Iskanderov <iskanderovofficial@gmail.com>
2024-07-03 21:46:53 +0500

    added badge component

--- src/components/favoritesBadge/FavoritesBadge.jsx
+++ src/components/favoritesBadge/FavoritesBadge.jsx
@@ -4,7 +4,7 @@ const FavoritesBadge = () => {
   const { favorites } = useFavorites();
   return (
     <span className="relative inline-block">
-      <span className="absolute top-0 right-0 inline-block w-4 h-4 text-xs font-bold text-white bg-red-500 rounded-full">
+      <span className="absolute -top-[20px] -right-[17px] flex justify-center items-center w-4 h-4 text-xs font-bold text-white bg-red-500 rounded-full">
         {favorites.length}
       </span>
     </span>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,11 +2,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import FavoritesBadge from '../favoritesBadge/FavoritesBadge';
+import logo from '../../images/logo.png';
 
 const Navbar = () => {
-  return (
-    <div></div>
-  );
+    return (
+        <div className='max-w-[1200px] mx-auto px-4 '>
+            <nav>
+                <div className="container mx-auto flex h-[80px] items-center justify-between">
+                    <Link to="/" className="font-semibold flex gap-2 items-center text-xl">
+                    <img src={logo} className='w-[80px] ' alt="" />
+                    Mango market</Link>
+                    <Link to="/favorites" className="font-medium text-xl relative">
+                        Favorites
+                        <FavoritesBadge />
+                    </Link>
+                </div>
+            </nav>
+        </div>
+    );
 };
 
 export default Navbar;
